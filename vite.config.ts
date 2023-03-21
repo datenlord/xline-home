105ff3b6ab27bee905fe301a76ed6fcf0eee9da8
Ying <cnfty786@gmail.com>
2023-03-21 20:16:33 +0800

    feat: configure path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const srcPath = resolve(__dirname, 'src')
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@/': `${srcPath}/`,
+    },
+  },
   plugins: [react()],
 })
