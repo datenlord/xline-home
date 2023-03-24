e52fb9b5f3ef44d6db20545295212b6249b42596
Ying <cnfty786@gmail.com>
2023-03-24 17:16:56 +0800

    feat: add react markdown dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
+    'react/no-children-prop': 'off',
     'react/no-unescaped-entities': 'off',
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
