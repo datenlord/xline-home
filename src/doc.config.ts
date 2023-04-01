1bf1a0a1c2785a2c8b7c79e2c727b200a546253f
Ying <cnfty786@gmail.com>
2023-04-01 17:00:09 +0800

    feat: add doc config file

---
+++ src/doc.config.ts
@@ -0,0 +1,170 @@
+export const docsConfig = [
+  {
+    title: "What's New",
+    url: "/docs/What's-New",
+    children: [
+      {
+        type: 'heading',
+        title: 'V0.1.0',
+        url: "/docs/What's-New",
+        id: 'V0.1.0',
+      },
+      {
+        type: 'heading',
+        title: 'V0.2.0',
+        url: "/docs/What's-New",
+        id: 'V0.2.0',
+      },
+      {
+        type: 'heading',
+        title: 'V0.3.0',
+        url: "/docs/What's-New",
+        id: 'V0.3.0',
+      },
+    ],
+  },
+  {
+    title: 'Get Started',
+    url: '/docs/Get-Started',
+    children: [
+      {
+        type: 'heading',
+        title: 'Architecture',
+        url: '/docs/Get-Started',
+        id: 'Architecture',
+      },
+      {
+        type: 'heading',
+        title: 'Xline In 5 Minutes',
+        url: '/docs/Get-Started',
+        id: 'Xline-In-5-Minutes',
+      },
+    ],
+  },
+  {
+    title: 'Deploy',
+    url: '/docs/Deploy',
+    children: [
+      {
+        type: 'heading',
+        title: 'Install',
+        url: '/docs/Deploy',
+        id: 'Install',
+      },
+      {
+        type: 'heading',
+        title: 'Performance Comparison',
+        url: '/docs/Deploy',
+        id: 'Performance-Comparison',
+      },
+    ],
+  },
+  {
+    title: 'Develop',
+    url: '/docs/Develop',
+    children: [
+      {
+        type: 'heading',
+        title: 'Xline API',
+        url: '/docs/Develop',
+        id: 'Xline-API',
+      },
+    ],
+  },
+  {
+    title: 'Reference',
+    url: '/docs/Xline-Architecture-Details',
+    children: [
+      {
+        type: 'article',
+        title: 'Xline Architecture Details',
+        url: '/docs/Xline-Architecture-Details',
+      },
+      {
+        type: 'article',
+        title: 'Performance Comparison',
+        url: '/docs/Performance-Comparison',
+      },
+    ],
+  },
+]
+
+export const deepDiveConfig = [
+  {
+    title: 'Consensus',
+    url: '/deep-dive/Consensus',
+    children: [
+      {
+        type: 'heading',
+        title: 'Basic Concept',
+        url: '/deep-dive/Consensus',
+        id: 'Basic-Concept',
+      },
+      {
+        type: 'heading',
+        title: 'Consensus Protocol',
+        url: '/deep-dive/Consensus',
+        id: 'Consensus-Protocol',
+      },
+    ],
+  },
+  {
+    title: 'KV Engine',
+    url: '/deep-dive/Key-value-Engine',
+    children: [
+      {
+        type: 'heading',
+        title: 'Metrics',
+        url: '/deep-dive/Key-value-Engine',
+        id: 'Metrics',
+      },
+      {
+        type: 'heading',
+        title: 'LSM Tree And B-Tree',
+        url: '/deep-dive/Key-value-Engine',
+        id: 'LSM-Tree-And-B-Tree',
+      },
+    ],
+  },
+  {
+    title: 'RPC',
+    url: '/deep-dive/Remote-Procedure-Calls-(RPC)',
+    children: [
+      {
+        type: 'heading',
+        title: 'Remote Procedure Call (RPC)',
+        url: '/deep-dive/Remote-Procedure-Calls-(RPC)',
+      },
+    ],
+  },
+  {
+    title: 'Test',
+    url: '/deep-dive/Testing',
+    children: [
+      {
+        type: 'heading',
+        title: 'Unit Testing',
+        url: '/deep-dive/Testing',
+        id: 'Unit-Testing',
+      },
+      {
+        type: 'heading',
+        title: 'Mock Testing',
+        url: '/deep-dive/Testing',
+        id: 'Mock-Testing',
+      },
+      {
+        type: 'heading',
+        title: 'Integration Testing',
+        url: '/deep-dive/Testing',
+        id: 'Integration-Testing',
+      },
+      {
+        type: 'heading',
+        title: 'Validation Testing',
+        url: '/deep-dive/Testing',
+        id: 'Validation-Testing',
+      },
+    ],
+  },
+]
