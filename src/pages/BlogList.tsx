5881075bc69735b7e3e88745d780efec6984d946
Ying <cnfty786@gmail.com>
2023-04-01 16:49:38 +0800

    fix:(BlogList): blog cover show

--- src/pages/BlogList.tsx
+++ src/pages/BlogList.tsx
@@ -102,7 +102,7 @@ const BlogListItem = styled.div`
 
 const BlogListItemCover = styled.img`
   width: 100%;
-  height: ${props => props.theme.scale.scale08};
+  /* height: ${props => props.theme.scale.scale08}; */
 `
 
 const BlogListItemContentContainer = styled.div`
