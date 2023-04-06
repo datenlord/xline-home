79e438565162176aefcf229ae3968fee5cc803e4
Ying <cnfty786@gmail.com>
2023-04-06 17:22:33 +0800

    feat: add router

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,46 @@
-import { createBrowserRouter } from 'react-router-dom'
-import styled from 'styled-components'
+import { createHashRouter } from 'react-router-dom'
+import LayoutPage from './pages/Layout'
+import HomePage from './pages/Home'
+import ContributePage from './pages/Contribute'
+import TestPage from './pages/Test'
+import DocsPage from './pages/Docs'
+import DeepDivePage from './pages/DeepDive'
+import BlogListPage from './pages/BlogList'
+import BlogDetailPage from './pages/BlogDetail'
 
-const StyledDiv = styled.div`
-  color: ${props => props.theme.color};
-`
-
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: '/',
-    element: <StyledDiv>Hello world!</StyledDiv>,
+    element: <LayoutPage />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/contribute',
+        element: <ContributePage />,
+      },
+      {
+        path: '/blog',
+        element: <BlogListPage />,
+      },
+      {
+        path: '/blog/:msg',
+        element: <BlogDetailPage />,
+      },
+      {
+        path: '/docs/:params',
+        element: <DocsPage />,
+      },
+      {
+        path: '/deep-dive/:params',
+        element: <DeepDivePage />,
+      },
+    ],
+  },
+  {
+    path: 'test',
+    element: <TestPage />,
   },
 ])
