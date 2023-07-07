80102c4a21d61561f94efe7cebaaa93b9718ac4b
LingKa <cnfty786@gmail.com>
2023-07-07 18:56:57 +0800

    feat(footer): add cncf logo & copyright section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,6 +7,8 @@ import logoUrl from '@/assets/logo.svg'
 import githubIconUrl from '@/assets/github-icon.svg'
 import gitterIconUrl from '@/assets/gitter-icon.svg'
 import DiscordIconUrl from '@/assets/discord-icon.svg'
+import CNCFLogoUrl from '@/assets/cncf-logo.png'
+import CNCFLogoTextUrl from '@/assets/cncf-logo-text.png'
 
 const FooterWrapper = styled.footer`
   background-image: url(${backgroundUrl});
@@ -81,8 +83,15 @@ const LinkListItem = styled.li`
   }
 `
 const FooterBottom = styled.div`
+  padding-bottom: 48px;
   display: flex;
   align-items: center;
+  @media screen and (max-width: 1024px) {
+    padding-bottom: 36px;
+  }
+  @media screen and (max-width: 768px) {
+    padding-bottom: 24px;
+  }
 `
 const Logo = styled.img`
   height: 48px;
@@ -136,6 +145,41 @@ const BottomLinkText = styled.p`
   @media screen and (max-width: 768px) {
     font-size: 12px;
   }
+`
+const CopyRightContainer = styled.div`
+  margin-left: -4%;
+  width: 108%;
+  padding-top: 32px;
+
+  color: #fff;
+  border-top: 1px solid #fff;
+`
+const CopyRightText = styled.div`
+  padding-bottom: 16px;
+  text-align: center;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 1;
+  &:last-child {
+    padding-bottom: 0;
+  }
+  @media screen and (max-width: 1024px) {
+    padding-bottom: 12px;
+    font-size: 13.5px;
+  }
+  @media screen and (max-width: 768px) {
+    padding-bottom: 8px;
+    font-size: 9px;
+  }
+`
+const CNCFLinkLogo = styled(BottomLinkContainer)`
+  cursor: default;
+`
+const CNCFLogo = styled.img`
+  height: 100%;
+`
+const CNCFLogoText = styled.img`
+  height: 100%;
 `
 
 export const Footer: React.FC = () => {
@@ -230,6 +274,10 @@ export const Footer: React.FC = () => {
               }}
             />
             <Placeholder />
+            <CNCFLinkLogo>
+              <CNCFLogo src={CNCFLogoUrl} />
+              <CNCFLogoText src={CNCFLogoTextUrl} />
+            </CNCFLinkLogo>
             <BottomLinkContainer
               as="a"
               href="https://github.com/datenlord/xline"
@@ -242,6 +290,12 @@ export const Footer: React.FC = () => {
               <BottomLinkText>Discord</BottomLinkText>
             </BottomLinkContainer>
           </FooterBottom>
+          <CopyRightContainer>
+            <CopyRightText>
+              We are a Cloud Native Computing Foundation sandbox project.
+            </CopyRightText>
+            <CopyRightText>Xline for The Linux Foundation®</CopyRightText>
+          </CopyRightContainer>
         </FooterContainer>
       </FooterWrapper>
     </>
